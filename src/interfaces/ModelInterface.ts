964ab72265a9f30da5c9f7ce6fb6f93c3727a992
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-07-10 12:13:27 -0300

    passa no requisito 1

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model<T> {
   create(data: T): Promise<T>;
   read(): Promise<T[]>;
-  readOne(id: string): Promise<T> | null;
-  update(id: string, data: T): Promise<T> | null;
-  delete(id: string): Promise<T> | null; 
+  readOne(id: string): Promise<T | null>;
+  update(id: string, data: T): Promise<T | null>;
+  delete(id: string): Promise<T | null>; 
 }
